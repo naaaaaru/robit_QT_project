adc0f9e1dff53ab86a2dc43dc88f2e811aa74583
naaaaaru <naru4231@gmail.com>
2024-10-15 21:29:50 +0900

    upload

--- chess_project/chess.cpp
+++ chess_project/chess.cpp
@@ -1,15 +1,19 @@
 #include "chess.h"
 #include "./ui_chess.h"
+#include <QBrush>
+#include <QPen>
 
 chess::chess(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::chess)
 {
     ui->setupUi(this);
 
-    // QGraphicsScene 초기화 및 체스판 그리기
+    // QGraphicsScene 초기화 및 설정
     scene = new QGraphicsScene(this);
     ui->graphicsView->setScene(scene);
-    drawChessBoard();
+
+    drawChessBoard();   // 체스판 그리기
+    placePieces();      // 체스말 배치
 
     // 초기 시간 표시 (5:00.00)
     updateLCD(whiteTimeRemaining, ui->white_timer);
@@ -25,18 +29,32 @@ chess::~chess()
     delete ui;
 }
 
-// 체스판 그리기
+// 체스판 그리기 (8x8 칸)
 void chess::drawChessBoard()
 {
-    int tileSize = 50;
-
+    int tileSize = 80;  // 각 칸의 크기를 80x80으로 설정
     for (int row = 0; row < 8; ++row) {
         for (int col = 0; col < 8; ++col) {
             QRectF tile(col * tileSize, row * tileSize, tileSize, tileSize);
-            QBrush brush((row + col) % 2 == 0 ? Qt::white : Qt::black);
+            QBrush brush((row + col) % 2 == 0 ? Qt::white : Qt::black);  // 번갈아 색 설정
             scene->addRect(tile, QPen(Qt::black), brush);
         }
     }
+}
+
+// 체스말을 주어진 위치에 배치하는 함수
+QGraphicsPixmapItem* chess::addPiece(const QString& imagePath, int row, int col)
+{
+    qDebug() << "Trying to load image from path:" << imagePath; // 추가 디버깅
+    QPixmap piece(imagePath);
+    if (piece.isNull()) {
+        qDebug() << "Failed to load image:" << imagePath;  // 여기에서 경로를 출력
+        return nullptr; // 혹은 적절한 오류 처리
+    }
+    piece = piece.scaled(80, 80);  // 말의 크기를 80x80으로 맞춤
+    QGraphicsPixmapItem* item = scene->addPixmap(piece);
+    item->setPos(col * 80, row * 80);  // 체스판 위치에 맞게 배치
+    return item;
 }
 
 // LCD 업데이트: 남은 시간을 분:초.밀리초 형식으로 표시
@@ -88,4 +106,46 @@ void chess::on_black_done_clicked()
 {
     blackTimer.stop();
     whiteTimer.start(10);  // 10ms마다 업데이트
+}
+
+// 체스판에 말 배치
+void chess::placePieces()
+{
+    // 흰색 말 배치
+    addPiece(":/images/whitepawn.jpg", 6, 0);
+    addPiece(":/images/whitepawn.jpg", 6, 1);
+    addPiece(":/images/whitepawn.jpg", 6, 2);
+    addPiece(":/images/whitepawn.jpg", 6, 3);
+    addPiece(":/images/whitepawn.jpg", 6, 4);
+    addPiece(":/images/whitepawn.jpg", 6, 5);
+    addPiece(":/images/whitepawn.jpg", 6, 6);
+    addPiece(":/images/whitepawn.jpg", 6, 7);
+
+    addPiece(":/images/whitecastle.jpg", 7, 0);
+    addPiece(":/images/whiteknight.jpg", 7, 1);
+    addPiece(":/images/whitebishop.jpg", 7, 2);
+    addPiece(":/images/whitequeen.jpg", 7, 3);
+    addPiece(":/images/whiteking.jpg", 7, 4);
+    addPiece(":/images/whitebishop.jpg", 7, 5);
+    addPiece(":/images/whiteknight.jpg", 7, 6);
+    addPiece(":/images/whitecastle.jpg", 7, 7);
+
+    // 검은색 말 배치
+    addPiece(":/images/blackpawn.jpg", 1, 0);
+    addPiece(":/images/blackpawn.jpg", 1, 1);
+    addPiece(":/images/blackpawn.jpg", 1, 2);
+    addPiece(":/images/blackpawn.jpg", 1, 3);
+    addPiece(":/images/blackpawn.jpg", 1, 4);
+    addPiece(":/images/blackpawn.jpg", 1, 5);
+    addPiece(":/images/blackpawn.jpg", 1, 6);
+    addPiece(":/images/blackpawn.jpg", 1, 7);
+
+    addPiece(":/images/blackcastle.jpg", 0, 0);
+    addPiece(":/images/blackknight.jpg", 0, 1);
+    addPiece(":/images/blackbishop.jpg", 0, 2);
+    addPiece(":/images/blackqueen.jpg", 0, 3);
+    addPiece(":/images/blackking.jpg", 0, 4);
+    addPiece(":/images/blackbishop.jpg", 0, 5);
+    addPiece(":/images/blackknight.jpg", 0, 6);
+    addPiece(":/images/blackcastle.jpg", 0, 7);
 }
--- chess_project/chess.h
+++ chess_project/chess.h
@@ -3,8 +3,10 @@
 
 #include <QMainWindow>
 #include <QGraphicsScene>
+#include <QGraphicsPixmapItem>  // 체스말 이미지 추가를 위한 헤더
+#include <QLCDNumber>
+#include <QDebug>
 #include <QTimer>
-#include <QLCDNumber>  // QLCDNumber 헤더 추가
 #include <QTime>
 
 QT_BEGIN_NAMESPACE
@@ -28,12 +30,14 @@ private slots:
 private:
     Ui::chess *ui;
     QGraphicsScene *scene;
+
+    void drawChessBoard();               // 체스판 그리기
+    void placePieces();  // 체스말 배치
     QTimer whiteTimer;
     QTimer blackTimer;
+    QGraphicsPixmapItem* addPiece(const QString& imagePath, int row, int col); // 체스말 추가
     int whiteTimeRemaining = 300000;  // 300초 = 5분 (밀리초 단위)
     int blackTimeRemaining = 300000;
-
-    void drawChessBoard();
     void updateLCD(int timeMs, QLCDNumber *lcd);
 };
 
